50ca3314f64c37e1cc9626d5aaeadeaf435e49d0
Prepe <37080825+Prepe@users.noreply.github.com>
2024-10-05 21:00:22 +0200

    Changed Player from temp to final art/size. Rebalanced movement stats

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,57 +1,58 @@
 extends CharacterBody2D
 class_name Player
 
-@export var gravity = 1500
-@export var jump_force = 350
-@export var backlash_force = 250
-@export var double_jump_force = 450
-@export var speed = 200
-@export var coyote_jump_time = 0.25
+@export var gravity = 3000
+@export var jump_force = 800
+@export var backlash_force = 750
+@export var double_jump_force = 700
+@export var speed = 700
+@export var grav_jump_delay = 0.25
 @export var double_jump = false
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 var active = true
-var coyote_timeout = 0.0
+var grav_jump_timeout = 0.0
 var can_double_jump = false
 var allowed_to_double_jump = false
 var backlash_X_force = 0.0
+var coyote_time = 0.0
 
 func _process(delta):
-	if coyote_timeout > 0:
-		coyote_timeout -= delta
+	if grav_jump_timeout > 0:
+		grav_jump_timeout -= delta
 	
-	if is_on_floor() == true && double_jump == true:
-		can_double_jump = true
-		allowed_to_double_jump = false
+	if is_on_floor() == true:
+		coyote_time = 0.25 
+		if double_jump == true:
+			can_double_jump = true
+			allowed_to_double_jump = false
+	elif coyote_time > 0.0:
+		coyote_time -= delta
 	
 	# gavity
-	if is_on_floor() == false && coyote_timeout <= 0.0:
+	if is_on_floor() == false && grav_jump_timeout <= 0.0:
 		velocity.y += gravity * delta
-		#if velocity.y >= 500 :
-		#	velocity.y = 500
 	
 	# movment
 	var direction = 0
 	if active:
 		if Input.is_action_pressed("jump"):
-			if is_on_floor():
+			if coyote_time > 0.0:
 				jump(jump_force)
-				coyote_timeout = coyote_jump_time
-			#elif coyote_timeout > 0:
-			#	jump(jump_force)
-			elif can_double_jump == true && allowed_to_double_jump == true:
+				grav_jump_timeout = grav_jump_delay
+			elif is_on_floor() == false && can_double_jump == true && allowed_to_double_jump == true:
 				can_double_jump = false
 				jump(double_jump_force)
-		elif coyote_timeout > 0.0:
-			coyote_timeout = 0.0
+		elif grav_jump_timeout > 0.0:
+			grav_jump_timeout = 0.0
 		elif is_on_floor() == false && can_double_jump == true:
 			allowed_to_double_jump = true
 		
 		direction = Input.get_axis("move_left","move_right")
 		
 	velocity.x = direction * speed + backlash_X_force
-	if abs(backlash_X_force) > 0.01: backlash_X_force *= 0.9
+	if abs(backlash_X_force) > 0.01: backlash_X_force *= 0.75
 	elif backlash_X_force != 0.0: backlash_X_force = 0.0
 	
 	move_and_slide()
@@ -84,5 +85,5 @@ func update_animations(direction):
 
 
 func _on_player_hit_area_hit_on_enemy(backlash: Vector2, dmg: float) -> void:
-	velocity.y += backlash.y * backlash_force
+	velocity.y = backlash.y * backlash_force
 	backlash_X_force += backlash.x * backlash_force
