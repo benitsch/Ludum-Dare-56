1c510ea3379720f3b21820372aba8c661d6cec6b
m1reyu <some.user@users.noreply.github.com>
2024-10-06 21:56:45 +0200

    modified level select to automatically generate level buttons

--- scripts/ui/level_select_button.gd
+++ scripts/ui/level_select_button.gd
@@ -4,7 +4,8 @@ extends SimpleButton
 @export var levelNumber: int = -1;
 
 func _ready() -> void:
-    if levelNumber > 0:
+    self.text = '%d' % levelNumber
+    if levelNumber > -1:
         var levelString: String = '%02d' % levelNumber
         self.targetScenePath = 'res://scenes/levels/level_' + levelString + '.tscn'
     super._ready()
---
+++ scripts/ui/level_select_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var grid: GridContainer = %LevelGrid
+
+var levelCount: int = 4
+var scene: Resource = preload('res://scenes/ui/level_select_button.tscn')
+
+func _ready() -> void:
+    if levelCount < 1:
+        return
+
+    for nLevel in range(1, levelCount + 1):
+        var instance = scene.instantiate()
+        if instance is LevelSelectButton:
+            instance.levelNumber = nLevel
+        grid.add_child(instance)
