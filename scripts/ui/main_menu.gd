7c93eafccafc4a504e471e2f31637fc636552a62
m1reyu <some.user@users.noreply.github.com>
2024-10-06 19:09:15 +0200

    fix wrong identing in some scripts

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -5,10 +5,10 @@ signal start_game()
 @export var startButton: NodePath;
 
 func _ready() -> void:
-	var startButtonNode = get_node(startButton)
-	if startButtonNode is Button:
-		startButtonNode.grab_focus()
+    var startButtonNode = get_node(startButton)
+    if startButtonNode is Button:
+        startButtonNode.grab_focus()
 
 func _on_start_button_pressed() -> void:
-	start_game.emit()
-	hide()
+    start_game.emit()
+    hide()
--- scripts/ui/simple_button.gd
+++ scripts/ui/simple_button.gd
@@ -5,17 +5,17 @@ extends Button
 @export_file('*.tscn') var targetScenePath: String = ''
 
 func _init() -> void:
-	self.alignment = HORIZONTAL_ALIGNMENT_LEFT
+    self.alignment = HORIZONTAL_ALIGNMENT_LEFT
 
 func _ready() -> void:
-	self.mouse_entered.connect(_on_button_mouse_entered)
-	self.pressed.connect(_on_button_pressed)
+    self.mouse_entered.connect(_on_button_mouse_entered)
+    self.pressed.connect(_on_button_pressed)
 
 func _on_button_mouse_entered() -> void:
-	grab_focus()
-	
+    grab_focus()
+    
 func _on_button_pressed() -> void:
-	if targetScenePath == '':
-		return
-	
-	get_tree().change_scene_to_file(targetScenePath)
+    if targetScenePath == '':
+        return
+    
+    get_tree().change_scene_to_file(targetScenePath)
