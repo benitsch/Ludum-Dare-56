7e5106f9c99e00d82e5c0d26569b689577042e8a
m1reyu <some.user@users.noreply.github.com>
2024-10-06 22:55:52 +0200

    adapt level select style

--- scripts/ui/level_select_button.gd
+++ scripts/ui/level_select_button.gd
@@ -1,11 +1,19 @@
 class_name LevelSelectButton
-extends SimpleButton
+extends TextureButton
 
 @export var levelNumber: int = -1;
+@onready var levelLabel: Label = $LevelLabel
 
 func _ready() -> void:
-    self.text = '%d' % levelNumber
-    if levelNumber > -1:
-        var levelString: String = '%02d' % levelNumber
-        self.targetScenePath = 'res://scenes/levels/level_' + levelString + '.tscn'
-    super._ready()
+    self.levelLabel.text = '%d' % levelNumber
+    self.pressed.connect(_on_button_pressed)
+
+    
+func _on_button_pressed() -> void:
+    if levelNumber < 0:
+        return
+
+    var levelString: String = '%02d' % levelNumber
+    var targetScenePath: String = 'res://scenes/levels/level_' + levelString + '.tscn'
+    
+    get_tree().change_scene_to_file(targetScenePath)
