a7c28e5f17d34dc54fb4e03487fcefa0dee8cb95
m1reyu <some.user@users.noreply.github.com>
2024-10-06 21:15:55 +0200

    export and extract the button text alignment to be easier maintainable

--- scripts/ui/level_select_button.gd
+++ scripts/ui/level_select_button.gd
@@ -3,9 +3,6 @@ extends SimpleButton
 
 @export var levelNumber: int = -1;
 
-func _init() -> void:
-    self.alignment = HORIZONTAL_ALIGNMENT_CENTER
-
 func _ready() -> void:
     if levelNumber > 0:
         var levelString: String = '%02d' % levelNumber
--- scripts/ui/simple_button.gd
+++ scripts/ui/simple_button.gd
@@ -1,8 +1,17 @@
 class_name SimpleButton
-
 extends Button
 
+enum AlignmentEnum {LEFT, CENTER, RIGHT}
+
 @export_file('*.tscn') var targetScenePath: String = ''
+@export var textAlignment: AlignmentEnum = AlignmentEnum.LEFT:
+    set(value):
+        if value == AlignmentEnum.CENTER:
+            self.alignment = HORIZONTAL_ALIGNMENT_CENTER
+        elif value == AlignmentEnum.RIGHT:
+            self.alignment = HORIZONTAL_ALIGNMENT_RIGHT
+        else:
+            self.alignment = HORIZONTAL_ALIGNMENT_LEFT
 
 func _init() -> void:
     self.alignment = HORIZONTAL_ALIGNMENT_LEFT
@@ -12,7 +21,9 @@ func _ready() -> void:
     self.pressed.connect(_on_button_pressed)
 
 func _on_button_mouse_entered() -> void:
-    grab_focus()
+    if self.has_focus() == false:
+        grab_focus()
+        var tmp = 0
     
 func _on_button_pressed() -> void:
     if targetScenePath == '':
